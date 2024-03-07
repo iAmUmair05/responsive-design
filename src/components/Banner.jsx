cf141527e77a78242a61c61c19768bab9b0b658c
Umair Younus <Hamadumair33@gmail.com>
2024-03-07 11:29:33 +0500

    add react type-animation pkg

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Typed from 'react-typed';
+import { TypeAnimation } from "react-type-animation";
 
 
 const Banner = () => {
@@ -12,13 +12,29 @@ const Banner = () => {
             <div className='text-white text-5xl md:text-[80px] font-bold mb-3'>Grow with us</div>
             <div className='font-bold text-4xl md:text-[50px] text-white mb-[63px]'>
 
-                <Typed className='pl-3'
+                {/* <Typed className='pl-3'
                     strings={["Web Development","Digital Marketing","Search Engine Optimization", "and Much More"]}
                     typeSpeed={100}
                     loop={true}
                     backSpeed={50}
-                />
+                /> */}
+
+                <TypeAnimation
+              sequence={[
+                "Web Development",
+                1000,
+                "Digital Marketing",
+                1000,
+                "Search Engine Optimization",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
 </div>
+
+
 
             <button className='bg-black text-white rounded w-[200px] p-3 hover:opacity-50'>Get Started</button>
         </div>
